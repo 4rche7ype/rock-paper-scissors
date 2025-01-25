af6347e4f5e4624e1561b90170f1ee347cc70d6b
Raymond <raydhz@pm.me>
2025-01-25 12:34:17 -0800

    Add a function for the computer's choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,20 @@
 console.log('hello');
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 10);
+    let comsPick = " ";
+
+    do {
+        randomNumber = Math.floor(Math.random() * 10);
+    } while (randomNumber == 0);
+
+    if (randomNumber >= 1 && randomNumber <= 3) {
+        comsPick = "rock";
+    } else if (randomNumber >= 4 && randomNumber <= 6) {
+        comsPick = "paper";
+    } else {
+        comsPick = "scissors";
+    }
+
+    return (comsPick);
+}
